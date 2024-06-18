2dfe05c5ce9ba36c038f45e7677ca8a8ea410733
Ilya Volchenkov <ivolchenkov@megaputer.ru>
2024-06-18 15:15:42 +0300

    Updated dependencies

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-import '@mantine/core/esm/index.css';
+import '@mantine/core/styles.css';
 
 import {
   MantineProvider,
